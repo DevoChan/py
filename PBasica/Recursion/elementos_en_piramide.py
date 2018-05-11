accafad81cf77e2e66799d913f84c977a3c06508
DevoChan <craftraxserver@gmail.com>
2018-05-10 23:00:49 -0700

    Recursion elementos piramide

---
+++ PBasica/Recursion/elementos_en_piramide.py
@@ -0,0 +1,9 @@
+import sys
+sys.setrecursionlimit(2002)
+
+def numero_elementos (tamano):
+    if tamano == 1: return 1
+    return tamano * tamano + numero_elementos(tamano-1)
+
+tamano = int(input("Introduce the piramid size: "))
+print(numero_elementos(tamano))
