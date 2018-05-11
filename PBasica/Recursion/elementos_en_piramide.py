1da0f4f220292d0b04736c7126edfd5ad1b015ec
DevoChan <37884242+DevoChan@users.noreply.github.com>
2018-05-10 23:07:40 -0700

    Update elementos_en_piramide.py

--- PBasica/Recursion/elementos_en_piramide.py
+++ PBasica/Recursion/elementos_en_piramide.py
@@ -5,5 +5,5 @@ def numero_elementos (tamano):
     if tamano == 1: return 1
     return tamano * tamano + numero_elementos(tamano-1)
 
-tamano = int(input("Introduce the piramid size: "))
+tamano = int(input("Write the piramid size: "))
 print(numero_elementos(tamano))
